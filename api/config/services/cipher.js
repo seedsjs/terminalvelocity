e12bc8aa6b1fdcb4b55573b33c5994a810360809
John-Henry Liberty <johnhenry.liberty@gmail.com>
2016-02-22 01:54:46 -0600

    add services

---
+++ api/config/services/cipher.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  services: {
+    cipher: {
+      jwt: {
+        secretKey: 'b3030dbaf1f93e3481570f4430bbd2b7b0a87568d29c2f17ac917b3c3ee43840'
+      }
+    }
+  }
+};
